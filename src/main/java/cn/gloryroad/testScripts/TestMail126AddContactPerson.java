195fa08154dd289cdb8cad149721d896254a3fc8
jamie <384505109@qq.com>
2021-09-08 22:14:38 +0800

    更新Excel

--- src/main/java/cn/gloryroad/testScripts/TestMail126AddContactPerson.java
+++ src/main/java/cn/gloryroad/testScripts/TestMail126AddContactPerson.java
@@ -7,11 +7,9 @@ import cn.gloryroad.util.Log;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -36,30 +34,98 @@ public class TestMail126AddContactPerson {
         System.setProperty("webdriver.firefox.bin", Constant.browswePath);
         webDriver = new FirefoxDriver();
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        webDriver.get(Constant.Url);
     }
 
-    @Test
-    public void testMail126AddContactPerson() throws Exception {
-        Log.startTestCase(ExcelUtil.getCellData(1, 0));
-        String username = ExcelUtil.getCellData(1, 1);
-        String password = ExcelUtil.getCellData(1, 2);
-        String contactName = ExcelUtil.getCellData(1, 3);
-        String email = ExcelUtil.getCellData(1, 4);
-        String phone = ExcelUtil.getCellData(1, 5);
+    /**
+     * 第一种方式，后续通过@DataProvider注解提供参数
+     *
+     * @Test public void testMail126AddContactPerson() throws Exception {
+     * Log.startTestCase(ExcelUtil.getCellData(1, 0));
+     * String username = ExcelUtil.getCellData(1, 1);
+     * String password = ExcelUtil.getCellData(1, 2);
+     * String contactName = ExcelUtil.getCellData(1, 3);
+     * String email = ExcelUtil.getCellData(1, 4);
+     * String phone = ExcelUtil.getCellData(1, 5);
+     * <p>
+     * Log.info("调用AddContactPersonAction的createContact方法");
+     * AddContactPersonAction.createContact(webDriver, username, password, contactName, email, phone);
+     * Log.info("调用 AddContactPerson_Action类的execute方法后，休眠 3 秒");
+     * Thread.sleep(3000);
+     * Log.info("断言通讯录的页面是否包含联系人姓名的关键字");
+     * Assert.assertTrue(webDriver.getPageSource().contains(contactName));
+     * Log.info("断言通讯录的页面是否包含联系人电子邮件地址的关键字");
+     * Assert.assertTrue(webDriver.getPageSource().contains(email));
+     * Log.info("断言通讯录的页面是否包含联系人手机号的关键字");
+     * Assert.assertTrue(webDriver.getPageSource().contains(phone));
+     * Log.info("新建联系人的全部断言成功，在 Excel 测试数据文件的“测试执行结果”列中写入“执行成功”");
+     * ExcelUtil.setCellData(1, 9, "success");
+     * Log.endTestCase(ExcelUtil.getCellData(1, 0));
+     * }
+     */
 
-        Log.info("调用AddContactPersonAction的createContact方法");
-        AddContactPersonAction.createContact(webDriver, username, password, contactName, email, phone);
-        Log.info("调用 AddContactPerson_Action类的execute方法后，休眠 3 秒");
-        Thread.sleep(3000);
-        Log.info("断言通讯录的页面是否包含联系人姓名的关键字");
-        Assert.assertTrue(webDriver.getPageSource().contains(contactName));
-        Log.info("断言通讯录的页面是否包含联系人电子邮件地址的关键字");
-        Assert.assertTrue(webDriver.getPageSource().contains(email));
-        Log.info("断言通讯录的页面是否包含联系人手机号的关键字");
-        Assert.assertTrue(webDriver.getPageSource().contains(phone));
-        Log.info("新建联系人的全部断言成功，在 Excel 测试数据文件的“测试执行结果”列中写入“执行成功”");
-        ExcelUtil.setCellData(1, 9, "success");
-        Log.endTestCase(ExcelUtil.getCellData(1, 0));
+    @DataProvider(name = "testData")
+    public Object[][] testData() throws IOException {
+        return ExcelUtil.getTestData(Constant.TestDataExcelFilePath, Constant.TestDataExcelFileSheet);
+    }
+
+    @Test(dataProvider = "testData")
+    public void testAddressBook(String caseRowNum, String testCaseName, String userName,
+                                String password, String contactName, String email, String contactPhone,
+                                String assertContactName, String assertEmail, String assertPhone) throws InterruptedException {
+        Log.startTestCase(testCaseName);
+
+        Log.info("执行AddContactPersonAction类的createPerson()");
+        try {
+            AddContactPersonAction.createContact(webDriver, userName, password, contactName, email, contactPhone);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.info("添加联系人失败");
+            /**因为Excel的序号格式默认为带一位小数，所以使用split(".")[0]取前面整数部分，
+             * 并传给setCellData()，在对应序号的测试数据行的最后一列设定为“测试执行失败”
+             */
+            ExcelUtil.setCellData(Integer.parseInt(caseRowNum.split(".")[0]), ExcelUtil.getLastColumnNum(), "测试执行失败");
+            //调用Assert类的fail将此测试用例设为执行失败，后续测试代码将不再执行
+            Assert.fail("执行AddContactPersonAction类的createPerson()失败");
+        }
+        Log.info("调用AddContactPersonAction类的createPerson()后，休眠2秒");
+        Thread.sleep(2000);
+
+        Log.info("判断通讯录的页面是否包含联系人姓名等关键字");
+        try {
+            Assert.assertTrue(webDriver.getPageSource().contains(assertContactName));
+        } catch (Exception e) {
+            Log.info("判断通讯录的页面是否包含联系人姓名等关键字失败");
+            ExcelUtil.setCellData(Integer.parseInt(caseRowNum.split(".")[0]), ExcelUtil.getLastColumnNum(), "测试执行失败");
+            Assert.fail("判断通讯录的页面是否包含联系人姓名等关键字失败");
+        }
+
+        Log.info("判断通讯录的页面是否包含联系人email等关键字");
+        try {
+            Assert.assertTrue(webDriver.getPageSource().contains(assertEmail));
+        } catch (Exception e) {
+            Log.info("判断通讯录的页面是否包含联系人email等关键字失败");
+            ExcelUtil.setCellData(Integer.parseInt(caseRowNum.split(".")[0]), ExcelUtil.getLastColumnNum(), "测试执行失败");
+            Assert.fail("判断通讯录的页面是否包含联系人email等关键字失败");
+        }
+
+        Log.info("判断通讯录的页面是否包含联系人电话等关键字");
+        try {
+            Assert.assertTrue(webDriver.getPageSource().contains(assertPhone));
+        } catch (Exception e) {
+            Log.info("判断通讯录的页面是否包含联系人电话等关键字失败");
+            ExcelUtil.setCellData(Integer.parseInt(caseRowNum.split(".")[0]), ExcelUtil.getLastColumnNum(), "测试执行失败");
+            Assert.fail("判断通讯录的页面是否包含联系人电话等关键字失败");
+        }
+
+        Log.info("新建联系人的全部断言成功，在Excel的“测试执行结果”列写入“执行成功”");
+        try {
+            Assert.assertTrue(webDriver.getPageSource().contains(assertContactName));
+        } catch (Exception e) {
+            ExcelUtil.setCellData(Integer.parseInt(caseRowNum.split(".")[0]), ExcelUtil.getLastColumnNum(), "执行成功");
+            Log.info("测试执行结果成功写入Excel数据文件的“测试执行结果”列");
+            Log.endTestCase(testCaseName);
+        }
     }
 
 
--- src/main/java/cn/gloryroad/util/ExcelUtil.java
+++ src/main/java/cn/gloryroad/util/ExcelUtil.java
@@ -1,17 +1,16 @@
 package cn.gloryroad.util;
 
-import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.DecimalFormat;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,7 +26,7 @@ public class ExcelUtil {
     private static XSSFCell cell;
 
     //设定要操作的Excel文件路径和Excel文件中的sheet名称
-    public static void setExcelFile(String path, String sheetName) throws Exception{
+    public static void setExcelFile(String path, String sheetName) throws Exception {
         FileInputStream excelFile;
         excelFile = new FileInputStream(path);
         excelBook = new XSSFWorkbook(excelFile);
@@ -64,7 +63,72 @@ public class ExcelUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
+
+    public static Object[][] getTestData(String excelFilePath, String sheetName) throws IOException {
+        //根据传入参数的数据文件和文件名称，组合出Excel文件的绝对路径
+        File file = new File(excelFilePath);
+        //读取Excel文件
+        FileInputStream read = new FileInputStream(file);
+
+        Workbook workbook = null;
+
+        //获取文件参数的拓展名，判断是xlsx还是xls
+        String fileExtensionName = excelFilePath.substring(excelFilePath.indexOf("."));
+        //文件类型如果是.xlsx，则使用XSSFWork实例化
+        //文件类型如果是.xlsx，则使用XSSFWork实例化
+        if (fileExtensionName.equals(".xlsx")) {
+            workbook = new XSSFWorkbook(read);
+        } else if (fileExtensionName.equals(".xls")) {
+            workbook = new HSSFWorkbook(read);
+        }
+        //通过sheetName生成sheet对象
+        Sheet sheet = workbook.getSheet(sheetName);
+
+        /**获取Excel文件sheet1中数据的行数：用getLastRowNum()获取数据最后一行行号，
+         用getFirstRowNum()获取第一行行号(),相减后算出数据的行数; 行列都是从0开始的*/
+        int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
+
+        //创建list对象来存储从Excel文件读取的内容
+        List<String[]> records = new ArrayList<>();
+        for (int i = 1; i <= rowCount; i++) {
+            //获取行对象
+            Row row = sheet.getRow(i);
+            /**用String[]存储Excel文件每行的数据，因为Excel最后一单元格为测试执行结果，
+             * 倒数第二个单元格为执行状态，这两列单元格不需要传入测试方法中，因此使用
+             * getLastCellNum()-2去掉后两个单元格数据
+             */
+            String[] fields = new String[row.getLastCellNum() - 1];
+
+            /**判断此条测试数据是否执行，Excel文件倒数第二列为数据行的状态位，“y”表示要执行，
+             * 非“y”表示不会参与测试脚本的执行，跳过数据
+             */
+            if (row.getCell(row.getLastCellNum() - 1).getStringCellValue().equals("y")) {
+                for (int j = 0; j <= row.getLastCellNum() - 2; j++) {
+                    if (row.getCell(j).getCellType() == CellType.STRING) {
+                        fields[j] = row.getCell(j).getStringCellValue();
+                    } else if (row.getCell(j).getCellType() == CellType.NUMERIC) {
+                        fields[j] = String.valueOf(Math.round(row.getCell(j).getNumericCellValue()));
+                    } else if (row.getCell(j).getCellType() == CellType.BOOLEAN) {
+                        fields[j] = String.valueOf(row.getCell(j).getBooleanCellValue());
+                    }
+                }
+                records.add(fields);
+            }
+        }
+        //定义函数返回值
+        Object[][] result = new Object[records.size()][];
+        //设置二维数组每行的值，每行是一个object对象
+        for (int i = 0; i < records.size(); i++) {
+            result[i] = records.get(i);
+        }
+        return result;
+    }
+
+    public static int getLastColumnNum() {
+        //返回Excel最后一列的列号，如果有12列，返回11（因为是从0开始的）
+        return excelSheet.getRow(0).getLastCellNum() - 1;
+    }
+
 
 }
