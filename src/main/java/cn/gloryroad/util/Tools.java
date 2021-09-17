24c2e3368a148028a8cc1f4421ed8e13b0246f34
jamie <384505109@qq.com>
2021-09-17 21:26:08 +0800

    update

--- src/main/java/cn/gloryroad/util/Tools.java
+++ src/main/java/cn/gloryroad/util/Tools.java
@@ -7,7 +7,7 @@ import java.net.URL;
 /**
  * @Author: Zhang Huijuan
  * @Date: 2021/9/12 13:05
- * 如果objectMap.properties文件在根目录下（Java项目），直接在page页面创建对象读取
+ * 如果objectMap.properties文件在根目录下（Java项目），直接在page页面创建Properties对象读取
  * 如果在resource文件夹下（maven项目），要设法获取文件路径
  */
 public class Tools {
